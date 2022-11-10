ab8c3a41bd6f3e9d55cd0cc38f88e6f4ae330df4
Marcos de Barros Vioque <marcos.debarrosvioque@gmail.com>
2022-11-10 15:19:22 +0100

    initial html structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body className="App">
+      <header>
+         <h1>Rick and Morty</h1>
       </header>
-    </div>
+      <main>
+         <section className="App-section">
+            <h2>Morty Smith</h2>
+            <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="Morty Smith" />
+            <button type="button">Learn more about Morty Smith</button>
+         </section>
+         <section className="App-section">
+            <h2>Rick Sanchez</h2>
+            <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="Rick Sanchez" />
+            <button type="button">Learn more about Rick Sanchez</button>
+         </section>
+      </main>
+      <footer>
+        <navbar className="Navbar">
+           <ul>
+            <li>
+              <a href="#home">Home
+              </a>
+            </li>
+            <li>
+              <a href="#favorites">Favorites
+              </a>
+            </li>
+            <li>
+              <a href="#random">Random
+              </a>
+            </li>
+            <li>
+              <a href="#other">Other
+              </a>
+            </li>
+          </ul>
+        </navbar>
+      </footer>
+    </body>
   );
 }
 
